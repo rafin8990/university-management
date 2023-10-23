3f3ce33ab4d68f36a9ae2590b1115eb98c640929
rafin8990 <rafin8990@gmail.com>
2023-10-24 03:02:30 +0600

    academic semister added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 import { userController } from './user.controller'
+import validateRequest from '../../middlewares/validateRequest'
+import { userValidation } from './user.validation'
 
 const router = express.Router()
 
-router.post('/create-user', userController.createUser)
+router.post(
+  '/create-user',
+  validateRequest(userValidation.createUserZodSchema),
+  userController.createUser,
+)
 
 export const userRoutes = router
