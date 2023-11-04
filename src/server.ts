3fbd48f0b8067d5c5b29ccc95c668c481dd9c991
rafin8990 <rafin8990@gmail.com>
2023-11-04 11:16:45 +0600

    package json file edited

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
-import { logger } from './shared/logger';
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    logger.info('database connected successfully');
+    console.log('database connected successfully');
     app.listen(config.port, () => {
       console.log(
         `university management project running on port ${config.port}`,
